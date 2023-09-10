083646ac7226ab65d90ea7b9bdfce95010c30488
ka <todo1@rotarydef.net>
2023-09-10 07:21:54 -0700

    fix miss

--- unit_test.cc
+++ unit_test.cc
@@ -1,20 +1,13 @@
 
 
 
-#ifdef TMAIN_KAUTIL_KAUTIL_HUGE_STATE_STATIC
+#ifdef TMAIN_KAUTIL_HUGE_STATE_STATIC
 
-//#include <stdio.h>
-//int tmain_kautil_kautil_huge_state_static(int(*)(const char*,...));
-//
-//int main(){
-//    return tmain_kautil_kautil_huge_state_static(printf);
-//}
-
+#include <stdio.h>
+int tmain_kautil_kautil_huge_state_static(int(*)(const char*,...));
 
 int main(){
-    
-    return 0;
-    
+    return tmain_kautil_kautil_huge_state_static(printf);
 }
 
 
